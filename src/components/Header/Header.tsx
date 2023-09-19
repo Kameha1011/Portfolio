8bef7adb016d4397d8a9e2267ff95f2f846c5a13
kameha1011 <omarcitofc2001@gmail.com>
2023-09-19 09:00:02 -0300

    header and nav component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import image from "../../assets/images/omar.webp";
+import "./Header.css";
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <section className="profile">
+        <img src={image} alt="profile picture" className="profile-pic" />
+        <h1>Omar Flores</h1>
+        <h3>Full Stack Web Developer</h3>
+        <p>
+          <i>
+            &quot;Welcome to the Web Developer Portfolio. Explore a collection
+            of projects, showcasing diverse skills and expertise.Unleash
+            creativity, powered by the color palette: vibrant red, deep teal,
+            lush green, warm orange, and soft peach. Navigate through the
+            journey of web development, where passion meets proficiency. Connect
+            with a world of possibilities. Let the code speak, and the designs
+            mesmerize. Embrace the art and science of web development. Engage,
+            Inspire, Create. &quot;
+          </i>
+        </p>
+      </section>
+    </header>
+  );
+};
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,13 @@
+export const Nav: React.FC = () => {
+    return (
+        <nav>
+            <ul>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Education</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
